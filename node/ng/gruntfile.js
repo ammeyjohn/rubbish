0bca4be4ae4a4b029a0dfc2c88869aff4bd84cc9
Patrick <ammeyjohn@gmail.com>
2014-12-07 21:03:26 +0800

    Add ng folder, which contains angular directives components.

---
+++ node/ng/gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                // define a string to put between each file in the concatenated output
+                separator: ';'
+            },
+            dist: {
+                // the files to concatenate
+                src: ['directives/**/*.js'],
+                // the location of the resulting JS file
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                // the banner is inserted at the top of the output
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        useminPrepare : {
+            html: 'index.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+        jshint: {
+            files: ['directives/**/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        usemin: {
+            html: 'index.html',
+            options: {
+                dest: 'dist'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-usemin');
+
+    grunt.registerTask('default', ['useminPrepare', 'jshint', 'concat', 'uglify', 'usemin']);
+};
