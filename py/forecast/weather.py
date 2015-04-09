f6d079ccc7c63e461e72fd121ca39d031cad8737
Patrick <ammeyjohn@qq.com>
2015-04-09 14:24:09 +0800

    Modify weather.py

--- py/forecast/weather.py
+++ py/forecast/weather.py
@@ -1,7 +1,8 @@
 __author__ = 'patrick'
 
 
-from datetime import datetime
+from datetime import date, datetime, timedelta
+from dateutil import parser
 from io import StringIO
 import urllib2
 import pandas as pd
@@ -12,18 +13,16 @@ ROOT_DOMAIN = 'http://www.wunderground.com'
 PART_DOMAIN = '/history/station'
 
 
-def get_url(code, date_str):
+def get_url(code, date):
     """Generate the url for weather downloading.
     :param code: The code of weather station.
-    :param date_str: The date string of weather to download.
+    :param date: The date string of weather to download.
     :return: Returns the url which point to the weather download page.
     """
 
-    d = datetime.strptime(date_str, '%Y-%m-%d')
-
     url = ROOT_DOMAIN + PART_DOMAIN
     url += '/%s' % code
-    url += '/%d/%d/%d' % (d.year, d.month, d.day)
+    url += '/%d/%d/%d' % (date.year, date.month, date.day)
     url += '/DailyHistory.html?format=1'
     return url
 
@@ -35,40 +34,45 @@ def download(url):
     """
 
     req = urllib2.Request(url)
-    html = urllib2.urlopen(req).read()
-    return html
-
-def download_to_csv(url):
-    """
-    :param url:
-    :return:
-    """
-    return pd.read_csv(url)
+    return urllib2.urlopen(req).read()
 
 
 if __name__ == '__main__':
 
     df = None
 
-    url =  get_url('zsss', '2013-04-25')
-    print 'Downloading from %s' % url
+    code = 'zsss'           # The code of weather station
+    sdate = parser.parse('2013-09-01')    # Start date
+    edate = parser.parse('2013-09-30')    # End date
+    cdate = sdate           # Current date
 
-    html = download(url)
-    html = html.strip()
-    html = html.replace('<br />', '')
-    print html
+    url_str = get_url(code, cdate)
+    print 'Downloading from %s' % url_str
 
-    str = StringIO(unicode(html))
-    print str
+    html_str = download(url_str)
+    html_str = html_str.strip()
+    html_str = html_str.replace('<br />', '')
+    print html_str
 
-    df = pd.DataFrame.from_csv(str)
-    rng = pd.date_range(start='2015-04-08', end='2015-04-09', freq='30min', closed='left')
-    df1 = df.reindex(rng)
-    print df1.head(60)
-    rng2 = pd.date_range(start='2013-04-25', end='2013-04-26', freq='30min', closed='left')
-    for r in rng2:
-        print r
+    # Create the DateFrame from csv string wrapped by StringIO
+    io_html = StringIO(unicode(html_str))
+    df0 = pd.DataFrame.from_csv(io_html)
+
+    # Arrange the weather data
+    st_today = date.today()
+    ed_today = st_today + timedelta(days=1)
+    rng1 = pd.date_range(start=st_today, end=ed_today, freq='30min', closed='left')
+    df1 = df0.reindex(rng1)
+    print df1.head()
+
+    # Reset datetime index
+    st_date = cdate
+    ed_date = st_date + timedelta(days=1)
+    rng2 = pd.date_range(start=st_date, end=ed_date, freq='30min', closed='left')
     df2 = df1.set_index(rng2)
     print df2.head()
+
+    if df is None:
+        df = df2
 
 
