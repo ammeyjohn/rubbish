8f54ea2d3f756e2d0416de49c4c978ae80874f2a
yuanjie <yuanjie@shanghai3.com>
2015-05-05 14:27:42 +0800

    main.py中实现样本数据的采集和整理； 增加相城水厂5月样本数据

--- py/forecast/main.py
+++ py/forecast/main.py
@@ -6,30 +6,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-if __name__ == '__main__':
+# Read data from csv files
+df1 = pd.read_csv('data/series_xc_1.csv', index_col=0, header=0, parse_dates=0)
+df2 = pd.read_csv('data/series_xc_2.csv', index_col=0, header=0, parse_dates=0)
 
-    start_date = date(2013, 5, 1)
-    end_date = date(2013, 6, 1)
+# Generate date range from start to end
+start_date = date(2014, 5, 1)
+end_date = date(2014, 5, 12)
+drange = pd.date_range(start_date, end_date, freq='1H', closed='left')
 
-    # Read data from csv files
-    df1 = pd.read_csv('data/series_xc_1.csv', index_col=[0], parse_dates=[0])
-    df2 = pd.read_csv('data/series_xc_2.csv', index_col=[0], parse_dates=[0])
+# Slice the sample data from start to end
+df1 = df1.reindex(drange)
+df1.columns = ['value1']
+print df1.head()
 
-    df1.head()
+df2 = df2.reindex(drange)
+df2.columns = ['value2']
+print df2.head()
 
-    # Create date ranges
-    drange = pd.date_range(start=start_date, end=end_date, freq='2min', closed='left')
+# Combine df1 and df2
+df = df1.join(df2)
+print df.head()
 
-    # Combine all data columns index with date
-    df = df1.reindex(drange)
-    df.head()
-    df = df.merge(df2.reindex(drange))
-
-    # DEBUG: head
-    df.head()
-
-    # Draw plot
-    df.plot()
-    plt.show()
-
-    pass
+# DEBUG: print plot
+df.plot()
+plt.show()
