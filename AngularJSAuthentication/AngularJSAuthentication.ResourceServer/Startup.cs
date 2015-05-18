10617240f04b4f48d5bf329be8b371fdd61286c4
yuanjie <yuanjie@shanghai3.com>
2015-05-18 13:49:31 +0800

    增加ResourceServer

---
+++ AngularJSAuthentication/AngularJSAuthentication.ResourceServer/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+
+[assembly: OwinStartup(typeof(AngularJSAuthentication.ResourceServer.Startup))]
+
+namespace AngularJSAuthentication.ResourceServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            //Token Consumption
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions
+            {
+            });
+        }
+    }
+}
